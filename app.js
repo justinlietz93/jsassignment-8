f566edb0a0fc8bbd1bac17313c297e87dc7a57b1
Justin Lietz <156801407+justinlietz93@users.noreply.github.com>
2024-04-13 17:50:44 -0500

    initial commit:

--- app.js
+++ app.js
@@ -20,11 +20,30 @@ app.get('/', (_, response) => {
 
 
 // GET /api/todos
+app.get('/api/todos', (request, response) => {
+    response.json(todos);
+});
 
 // POST /api/todos
+app.post('/api/todos', (request, response) => {
+    const { item } = request.body;
+    const id = todos.length + 1;
+    const complete = false;
+    todos.push({ id, item, complete });
+    response.status(201).json({ id });
+});
 
 // PUT /api/todos/:id
-
+app.put('/api/todos/:id', (request, response) => {
+    const { id } = request.params;
+    const task = todos.find(todo => todo.id.toString() === id);
+    if (task) {
+        task.complete = !task.complete;
+        response.json({ id: task.id, complete: task.complete });
+    } else {
+        response.status(404).json({ message: 'Task not found' });
+    }
+});
 
 
 const message = `Server running: http://localhost:${port}`
